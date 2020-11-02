2422ddd514aecb86b90c3f0a54f168e86c038f8d
Sazib uddin <sazibuddin19@yahoo.com>
2020-11-02 10:40:52 +0600

    changes

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,7 @@
         <h1>Todos</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam perspiciatis dolorum minus cum. Provident eos laborum, architecto in sequi doloribus tempore asperiores itaque totam voluptatem ea accusantium a! Quod.</p>
         <div v-for="todo in todos" :key="todo.id">
-            <h3> lorem </h3>
+            <h3>{{ todo.title }} </h3>
         </div>
     </div>
 </template>
